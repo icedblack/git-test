c87cd5ed8edfe1f221e7b91b787b2ad439170184
yangc <861808733@qq.com>
2024-03-24 10:24:40 +0800

    22

--- code1.js
+++ code1.js
@@ -2,7 +2,7 @@ console.log(111)
 function debounce(fn,time){
     let timer;
     return function(){
-        clearTimeout(timer);
+        clearTimeout(timer);//清除定时器
         timer = setTimeout(()=>{
             fn.applay(this,...arguments)
         },time)
