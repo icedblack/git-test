9d462e859a828fdc7f8929cc55ba2be35935c209
yangc <861808733@qq.com>
2024-04-06 18:34:20 +0800

    dev分支的

--- code1.js
+++ code1.js
@@ -2,6 +2,7 @@ console.log(111)
 function debounce(fn,time){
     let timer;
     console.log('提交方法测试');
+    console.log('dev分支');
     return function(){
         clearTimeout(timer);//清除定时器
         timer = setTimeout(()=>{
